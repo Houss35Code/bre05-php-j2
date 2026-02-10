091805d40e9d5ba0e779586ef0177af51abd0830
Houss35Code <h.halifa035@gmail.com>
2026-02-10 11:14:56 +0100

    Exercice : Utilisation de require

---
+++ exercice-require/index.php
@@ -0,0 +1,11 @@
+<?php
+// ÉTAPE 3 : Dynamiser les contenus
+$siteTitle = "J'aime pas les lundis";
+$pageTitle = "Qui est Garfield ?";
+$pageContent = "Un chat roux qui aime les lasagnes.";
+
+// ÉTAPE 1 : Appeler le layout principal
+// Les variables ci-dessus seront disponibles dans layout.phtml
+// et dans tous les fichiers qu'il inclut
+require 'templates/layout.phtml';
+?>
